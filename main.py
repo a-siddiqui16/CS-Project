8002ddcc2b93d2754255b5624b7ecaed2d4996af
Abdur-Rahman Siddiqui <mailabdurrahmans@gmail.com>
2025-10-14 19:48:25 +0100

    GUI

--- main.py
+++ main.py
@@ -1,3 +1,37 @@
-print("Hello")
-print("This is a test")
-print("aaaaaa")
+#Imports
+
+import tkinter as tk
+
+
+#Login Page
+
+window = tk.Tk()
+
+#Title
+window.title("Login form")
+
+#Default Resolution
+window.geometry("1920x1080")
+
+#Background Colour
+window.configure(bg="#FFFFFF")
+
+#Creating Widgets
+login_label = tk.Label(window, text="Login")
+username_label = tk.Label(window, text="Username")
+username_entry = tk.Entry(window)
+password_entry = tk.Entry(window, show="*")
+password_label - tk.Label(window, text="Password")
+login_button = tk.Button(window, text="Login")
+
+#Placing widgets on screen
+login_label
+
+
+
+
+
+
+
+
+window.mainloop()
